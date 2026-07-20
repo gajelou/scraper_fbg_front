e013b6e6f8f607aec8baded55e8289be25b20bc3
gajelou <125290990+gajelou@users.noreply.github.com>
2026-07-20 17:18:26 -0300

    Update API_URL to new endpoint

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { ref, onMounted } from "vue";
 
-const API_URL = "https://hold-attachments-indexed-filters.trycloudflare.com";
+const API_URL = "https://environment-companies-phi-finance.trycloudflare.com";
 
 const agio = ref<number>(30);
 const mostrarPrecos = ref(true);
